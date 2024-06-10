71d88040d462a1706d62ad19e7c6bca9177a2f57
Rupam3220 <rupam.3220@gmail.com>
2024-06-10 17:53:13 +0530

    folder structure

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const Login = () => {
+
+  const user = ""
+  // const [register, handleSubmit, formState: {errors}] = useForm()
+
+  // useEffect(() => {
+  //   first
+  
+  //   return () => {
+  //     second
+  //   }
+  // }, [third])
+  
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default Login
